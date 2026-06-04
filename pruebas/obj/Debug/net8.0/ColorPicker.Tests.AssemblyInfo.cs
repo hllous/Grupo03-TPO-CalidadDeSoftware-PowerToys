9431005e281b372251a22758fd3455641b8972f7
Santiago Larre <santiagolarre.98@gmail.com>
2026-06-04 11:34:22 -0300

    Sumo los tests unitarios para color picker y quick accent. Ademas los tests de Integracion

--- pruebas/obj/Debug/net8.0/ColorPicker.Tests.AssemblyInfo.cs
+++ pruebas/obj/Debug/net8.0/ColorPicker.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ColorPicker.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2f213cb7857e1c5db3d41eec73ce0d006f7f7d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2adf353039b27cb75d5f70d3c3824365df718b09")]
 [assembly: System.Reflection.AssemblyProductAttribute("ColorPicker.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ColorPicker.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
